1ad0848a34569ccec59d5478b2d54acc3d02ade7
Anwe119 <rytter123>
2015-02-07 21:24:21 +0100

    hidden text nearly done

---
+++ NGJ15/Assets/Scripts/HiddenText.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HiddenText : MonoBehaviour {
+
+	private float alpha;
+
+	void OnEnable()
+	{
+		this.renderer.enabled = true;
+		alpha = renderer.material.GetFloat ("_Cutoff");
+		StartCoroutine (ShowText());
+	}
+
+	IEnumerator ShowText() {
+		float time = 0;
+		while (renderer.material.GetFloat("_Cutoff") >= 0.2) {
+			time += Time.deltaTime;
+			renderer.material.SetFloat("_Cutoff", Mathf.Lerp (alpha, 0.2f, time));
+			// Yield execution of this coroutine and return to the main loop until next frame
+			yield return null;
+		}
+
+	}
+}
