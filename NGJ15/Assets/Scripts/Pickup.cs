9cc9a51a660f79dbe9f6c7c05d07368696427d2c
Anwe119 <rytter123>
2015-02-07 09:17:51 +0100

    Pickup works as intended now

--- NGJ15/Assets/Scripts/Pickup.cs
+++ NGJ15/Assets/Scripts/Pickup.cs
@@ -11,25 +11,30 @@ public class Pickup : MonoBehaviour {
 	private bool handsFull = false;
 	// Use this for initialization
 	void Start () {
-		hand = this.transform.parent.Find("Hand").gameObject;
+		hand = this.transform.GetChild(0).gameObject;
 	}
 	
 	// shouold be remade into two functions so that it looks nice and needs to disable some stuff when picking up and putting down Also i have to mone it out a bit from the character so that you can allways move forward
 	void Update () {
 		if(Input.GetMouseButtonDown (0) && hand.transform.childCount !=0)
 		{
-			hand.transform.GetChild(0).rigidbody.useGravity = true;
-			hand.transform.GetChild(0).rigidbody.isKinematic = false;
-			hand.transform.GetChild(0).parent = null;
+			GameObject item = hand.transform.GetChild(0).gameObject;
+			item.rigidbody.useGravity = true;
+			item.rigidbody.constraints = RigidbodyConstraints.None;
+			item.transform.parent = null;
 		}
 		if (Input.GetMouseButtonDown (0) && !handsFull) // get left mouse button down (only one frame)
 		{
 			RaycastHit hit;
 			if (Physics.Raycast(transform.position, this.transform.forward, out hit, PickupDistance))
 			{
+				if(hit.transform.GetComponent<Usable>() != null)
+				{
 				hit.transform.parent = hand.transform;
+
+				hit.rigidbody.constraints = RigidbodyConstraints.FreezeAll;
 				hit.rigidbody.useGravity = false;
-				hand.transform.GetChild(0).rigidbody.isKinematic = true;
+				}
 			}
 			Debug.DrawRay(transform.position, this.transform.forward*PickupDistance, Color.green,10.0f );
 		}
