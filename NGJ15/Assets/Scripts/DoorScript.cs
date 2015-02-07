1d61eb3b5fea91f29ed1f3bb0b7fea6195e7e883
Anwe119 <rytter123>
2015-02-08 00:02:06 +0100

    soem dore script ring can now be picked up

---
+++ NGJ15/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorScript : MonoBehaviour {
+
+	private FadeScript fader;
+
+	// Use this for initialization
+	void Start () {
+		fader = GameObject.FindObjectOfType<FadeScript>();
+	}
+	
+	// Update is called once per frame
+	void OnCollisionEnter (Collision other) {
+		if (other.transform.tag == "key") {
+						fader.FadeToBlack ();
+				} else if (other.transform.GetComponent<Flamable> () != null)
+						other.transform.GetComponent<Flamable> ().FlameTimer = 10.0f;
+	}
+}
