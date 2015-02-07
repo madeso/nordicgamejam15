8f647e7e77b74a367740f34e309fd1a80baf4bc2
Anwe119 <rytter123>
2015-02-07 10:42:26 +0100

    Now we can throw usable stuff

--- NGJ15/Assets/Scripts/Pickup.cs
+++ NGJ15/Assets/Scripts/Pickup.cs
@@ -6,9 +6,21 @@ public class Pickup : MonoBehaviour {
 	[SerializeField]
 	private float PickupDistance;
 
+	[SerializeField]
+	private float ThrowIncreaseSpeed = 20;
+
+	[SerializeField]
+	private float maxThrowForce = 50;
+
+	[SerializeField]
+	private float minThrowForce = 1;
+
 	private GameObject hand;
 	private Camera camera;
 	private bool handsFull = false;
+	private GameObject item;
+	private float throwForce = 0;
+
 	// Use this for initialization
 	void Start () {
 		hand = this.transform.GetChild(0).gameObject;
@@ -16,14 +28,14 @@ public class Pickup : MonoBehaviour {
 	
 	// shouold be remade into two functions so that it looks nice and needs to disable some stuff when picking up and putting down Also i have to mone it out a bit from the character so that you can allways move forward
 	void Update () {
-		if(Input.GetMouseButtonDown (0) && hand.transform.childCount !=0) // when we have an item and pres the left mouse button we set it free
+
+		if(Input.GetMouseButtonDown (1) && hand.transform.childCount !=0) // when we have an item and pres the left mouse button we set it free
 		{
-			GameObject item = hand.transform.GetChild(0).gameObject;   // the object
-			item.rigidbody.useGravity = true;  //enables gravity
-			item.rigidbody.constraints = RigidbodyConstraints.None; //release all constrains
-			item.transform.parent = null; // set it free!!!
+			LetGo();
+			throwForce = 0; // resets force so that the player cannot save up force
 		}
-		if (Input.GetMouseButtonDown (0) && !handsFull) // get left mouse button down (only one frame)
+
+		if (Input.GetMouseButtonUp (0) && !handsFull) // get left mouse button down (only one frame)
 		{
 			RaycastHit hit;
 			if (Physics.Raycast(transform.position, this.transform.forward, out hit, PickupDistance)) // sends a ray to find any object
@@ -31,24 +43,52 @@ public class Pickup : MonoBehaviour {
 				if(hit.transform.GetComponent<Usable>() != null) // if we hit an object and it does not have a usable script on it, ignore it
 				{
 				hit.transform.parent = hand.transform; // move in as a child of hand
-
+				item = hand.transform.GetChild(0).gameObject;   // the object
 				hit.rigidbody.constraints = RigidbodyConstraints.FreezeAll; // freez rotation and position of the rigidbody
 				hit.rigidbody.useGravity = false;  // disable gravity so that we can lift it
 				}
 			}
-			Debug.DrawRay(transform.position, this.transform.forward*PickupDistance, Color.green,1f ); // debug ray for seeing the distance
+			Debug.DrawRay(transform.position, this.transform.forward*PickupDistance, Color.green, 1f); // debug ray for seeing the distance
 		}
 
+		if(Input.GetMouseButton(0) && handsFull && throwForce < maxThrowForce) // If left mous botton is down we check if there is something in our hands and that the force is not above max force before adding more.
+		{
+				throwForce += Time.deltaTime*ThrowIncreaseSpeed;
+		}
+
+		if(Input.GetMouseButtonUp(0) && throwForce > minThrowForce && handsFull) // checking for a left mouse up input and ensures that the force is sufficient for a throw 
+		{
+			Throw();
+		}
 
 		//anything picked up???
-		if (hand.transform.childCount != 0) 
+		if (hand.transform.childCount != 0)
 		{
 			handsFull = true; //yes
 		}
 		else
 		{
 			handsFull = false; //no
+			item = null;
 		}
+
+	}
+
+	private void LetGo() // ensures that we return the object to its original state
+	{
+		if (item != null) 
+		{
+						item.rigidbody.useGravity = true;  //enables gravity
+						item.rigidbody.constraints = RigidbodyConstraints.None; //release all constrains
+						item.transform.parent = null; 		 // set it free!!!
+		}
+	}
+
+	private void Throw() // adds force before letting it go
+	{
+		item.rigidbody.AddForce (this.transform.forward*throwForce, ForceMode.Impulse);
+		LetGo ();
+		throwForce = 0; // resets force.
 
 	}
 
---
+++ NGJ15/Assets/Standard Assets/Character Controllers/Sources/Scripts/Flammable.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flammable : MonoBehaviour {
+
+	[SerializeField]
+	private float flamable = 0; //0<= is no fire above sets the time in miliseconds.
+	
+	[SerializeField]
+	private GameObject fireParticles; // perticle prefab
+	
+	IEnumerator Burn(float flammable, GameObject Particles) // to 
+	{
+		//instantiate fire on object position as a child of it
+		
+		yield return new WaitForSeconds(flamable);
+		//destroy Particles
+	}
+}
