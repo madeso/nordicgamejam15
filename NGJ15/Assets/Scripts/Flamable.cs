6be6886804aefc874e31ca4c9a858ac2da9bdd77
Gustav <sir.gustav.the.coder@gmail.com>
2015-02-07 13:00:14 +0100

    minor fixes

--- NGJ15/Assets/Scripts/Flamable.cs
+++ NGJ15/Assets/Scripts/Flamable.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class Flamable : MonoBehaviour {
 
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	// To unity
+
 	/// <summary>
 	/// Set to -1 to make it burn forever. Greater than 0, seconds left of burn time.
 	/// </summary>
@@ -13,16 +16,37 @@ public class Flamable : MonoBehaviour {
 	/// </summary>
 	public Color CoalColor = Color.black;
 
-	private const float SECONDS_TO_COAL = 3; // how many seconds it will take before the object will turn to coal
-	private const float BURN_DAMAGE = 1.0f; // the ammount of damage applied
+	/// <summary>
+	/// How many seconds it will take before the object will turn to coal
+	/// </summary>
+	public float SecondsToCoal = 3;
 
-	private const float SECONDS_BETWEEN_DAMAGE = 1; // time in seconds between hurt calls
+	/// <summary>
+	/// The ammount of damage applied each burn update
+	/// </summary>
+	public float BurnDamage = 1.0f;
+
+	/// <summary>
+	/// Time in seconds between each burn update (hurt/damage call)
+	/// </summary>
+	public float SecondsBetweenDamage = 1;
+
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	// High level logic
 
 	/// <summary>
 	/// Called when it should start burning
 	/// </summary>
 	private void StartBurning() {
 		Debug.Log(string.Format("{0}: started burning", this.gameObject.name));
+	}
+
+	/// <summary>
+	/// Called each burn update so we can play some sound or something...
+	/// </summary>
+	private void OnBurn() {
+		// todo: play some burn sound
+		Debug.Log(string.Format("{0}: playing some burn sound...", this.gameObject.name));
 	}
 	
 	/// <summary>
@@ -41,9 +65,13 @@ public class Flamable : MonoBehaviour {
 	private const float INFINITE_BURN_VALUE = -0.9f;
 	private const float BURNING_STOPPED = -0.5f;
 
-	private const float COAL_CHANGE_PER_SECOND = 1 / SECONDS_TO_COAL;
+	private float CoalChangePerSecond  {
+		get {
+			return 1 / SecondsToCoal;
+		}
+	}
 
-	private float timeUntilNextDamage = SECONDS_BETWEEN_DAMAGE;
+	private float timeUntilNextDamage = 15;
 
 	private Color startColor;
 	private float isCoal = 0; // 0=not coal, 1=coal
@@ -65,7 +93,7 @@ public class Flamable : MonoBehaviour {
 
 	void Start () {
 		this.startColor = this.gameObject.renderer.material.color;
-		this.timeUntilNextDamage = Random.Range(0, SECONDS_BETWEEN_DAMAGE); // randomize it
+		this.timeUntilNextDamage = Random.Range(0, SecondsBetweenDamage); // randomize it
 		if( this.IsBurning ) {
 			StartBurning();
 		}
@@ -76,18 +104,19 @@ public class Flamable : MonoBehaviour {
 		if( this.IsBurning ) {
 			this.timeUntilNextDamage -= Time.deltaTime;
 			if( this.timeUntilNextDamage <= 0 ){
-				this.timeUntilNextDamage += SECONDS_BETWEEN_DAMAGE;
+				this.timeUntilNextDamage += SecondsBetweenDamage;
 				var dest = this.gameObject.GetComponent<Destructible>();
 				if( dest != null ) {
-					dest.Hurt(BURN_DAMAGE);
 					Debug.Log(string.Format("{0}: burn dmg hurting", this.gameObject.name));
+					dest.Hurt(BurnDamage);
+					this.OnBurn();
 				}
 			}
 		}
 
 		// change the color of the object to a more coal like black
 		if( this.IsBurning ) {
-			var coalChange = Time.deltaTime * COAL_CHANGE_PER_SECOND;
+			var coalChange = Time.deltaTime * CoalChangePerSecond;
 			this.isCoal = Mathf.Clamp01(this.isCoal + coalChange);
 			this.renderer.material.color = Color.Lerp (this.startColor, this.CoalColor, this.isCoal);
 		}
