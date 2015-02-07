1bddc4312890091559270a5bbbd89bacc745c3a9
Anwe119 <rytter123>
2015-02-07 19:57:58 +0100

    ring in bear prefab

--- NGJ15/Assets/Scripts/Fire.cs
+++ NGJ15/Assets/Scripts/Fire.cs
@@ -3,12 +3,12 @@ using System.Collections;
 
 public class Fire : MonoBehaviour {
 
-	void OnCollisionStay(Collision other)
+	void OnTriggerEnter(Collider other)
 	{
 		if(other.transform.GetComponent<Flamable>() != null)
 		{
 			Debug.Log("FIRE");
-			other.transform.GetComponent<Flamable>().FlameTimer = 2.0f;
+			other.transform.GetComponent<Flamable>().FlameTimer = 10f;
 		}
 	}
 }
