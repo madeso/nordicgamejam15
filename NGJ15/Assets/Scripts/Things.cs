080c0206085d5f547531f597951f99c0c4172806
Anwe119 <rytter123>
2015-02-07 02:56:47 +0100

    Usable and objects

---
+++ NGJ15/Assets/Scripts/Things.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Things : MonoBehaviour {
+	
+	[SerializeField]
+	AudioClip clip; //the sound to play.
+	
+	void HitSound(AudioClip sound)
+	{
+		//play audioclip
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if(other.gameObject.GetComponent<Usable>() != null)
+		{
+			HitSound(clip); // play sound
+		}
+	}
+}
---
+++ NGJ15/Assets/Scripts/Usable.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Usable : Things {
+
+	[SerializeField]
+	private float flamable = 0; //0<= is no fire above sets the time in miliseconds.
+
+	[SerializeField]
+	private bool inDestructable = false; // can be destroyed
+
+	[SerializeField]
+	private GameObject fireParticles; // perticle prefab
+
+	private bool thrown = false; //to check if the object was thrown
+	
+	IEnumerator Burn(float flammable, GameObject Particles) // to 
+	{
+		//instantiate fire on object position as a child of it
+		
+		yield return new WaitForSeconds(flamable);
+		//destroy Particles
+	}
+
+	void wrecking (GameObject toBeWrecked)
+	{
+		Debug.Log ("wrecking is still under construction");
+		//disable this model and replace with breakable version found in the parent object's child.
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if(other.gameObject.GetComponent<Usable>() != null)
+		{
+			Usable useable = other.gameObject.GetComponent<Usable>();
+			if(useable.thrown)
+			{
+				if(!useable.inDestructable)
+				{
+					wrecking(other.gameObject);
+				}
+				wrecking(this.gameObject);
+			}
+		}
+
+	}
+}
