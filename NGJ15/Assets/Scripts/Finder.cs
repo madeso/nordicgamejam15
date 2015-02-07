0ab4834c6ec6619873b3db2a373148e9fa1c31de
Anwe119 <rytter123>
2015-02-07 22:53:20 +0100

    Now the text works with teh light

---
+++ NGJ15/Assets/Scripts/Finder.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Finder : MonoBehaviour {
+
+	//[SerializeField]
+	private float lightDistance = 10.0f; 
+
+	private Light torch;
+
+	void Start()
+	{
+		torch = this.GetComponentInChildren<Light> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+		if(this.transform.parent != null)
+		{
+			if(this.transform.parent.name == "Hand")
+			{
+			torch.enabled = true;
+
+			RaycastHit hit;
+			if (Physics.Raycast(transform.position, this.transform.forward, out hit, lightDistance)) // sends a ray to find any object
+			{
+				if(hit.transform.gameObject.GetComponent<HiddenText>() != null)
+				{
+					hit.transform.gameObject.GetComponent<HiddenText>().enabled = true;
+				}
+					Debug.DrawRay(transform.position, this.transform.up *lightDistance, Color.green, 1f);
+			}
+			}
+		}
+
+	}
+}
