b40558f512d40164b5ad40b8bc962379844d6ad1
Anwe119 <rytter123>
2015-02-08 11:18:52 +0100

    Ring with light and nopass good

--- NGJ15/Assets/Scripts/NOpassingScript.cs
+++ NGJ15/Assets/Scripts/NOpassingScript.cs
@@ -4,8 +4,9 @@ using System.Collections;
 public class NOpassingScript : MonoBehaviour {
 
 	void OnCollisionEnter(Collision other){
-		if (other.transform.name = "keyHole") 
+		if (other.transform.name == "keyHole") 
 		{
+			this.collider.enabled = false;
 			this.GetComponent<Flamable>().FlameTimer = 5.0f;
 			audio.Play();
 		}
