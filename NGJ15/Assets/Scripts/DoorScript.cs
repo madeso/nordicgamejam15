1736d89c265e05eaffa1e7edb100e954b8b33ab0
Anwe119 <rytter123>
2015-02-08 11:34:32 +0100

    Door can now show things when opened

--- NGJ15/Assets/Scripts/DoorScript.cs
+++ NGJ15/Assets/Scripts/DoorScript.cs
@@ -4,18 +4,26 @@ using System.Collections;
 public class DoorScript : MonoBehaviour {
 
 	private FadeScript fader;
+	
+	public GameObject objectToShow;
 
 	// Use this for initialization
 	void Start () {
 		fader = GameObject.FindObjectOfType<FadeScript>();
+		if(objectToShow != null){
+			objectToShow.renderer.enabled = false;
+		}
 	}
 	
 	// Update is called once per frame
 	void OnCollisionEnter (Collision other) {
 		if (other.transform.tag == "Key") {
+			if(objectToShow != null){
+				objectToShow.renderer.enabled = true;
+			}
 						fader.FadeToBlack();
 			Application.LoadLevel(0);
-				} else if (other.transform.GetComponent<Flamable> () != null)
-						other.transform.GetComponent<Flamable> ().FlameTimer = 10.0f;
+				}
+
 	}
 }
