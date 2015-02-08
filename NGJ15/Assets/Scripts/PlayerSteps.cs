802e0948bf45e203f63f038a861a23e22f8bdf77
Gustav <sir.gustav.the.coder@gmail.com>
2015-02-08 13:37:21 +0100

    Player stepping logic

---
+++ NGJ15/Assets/Scripts/PlayerSteps.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSteps : MonoBehaviour {
+	private float stepsdim = 0;
+
+	/// <summary>
+	/// Time between the steps...
+	/// </summary>
+	public float Steptime = 0.4f;
+	private Vector3 lastPos;
+	// Use this for initialization
+	void Start () {
+		lastPos = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( (lastPos-this.transform.position).sqrMagnitude > 0.0f ) {
+			stepsdim += Time.deltaTime;
+			this.lastPos = this.transform.position;
+		}
+
+		if( this.stepsdim >= this.Steptime ) {
+			this.audio.Play();
+			this.stepsdim -= this.Steptime;
+		}
+	}
+}
