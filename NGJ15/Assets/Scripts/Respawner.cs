fa170be5968c9f18b5175ce1d4b89f1ed25ead41
Anwe119 <rytter123>
2015-02-08 12:04:08 +0100

    respawner

--- NGJ15/Assets/Scripts/Respawner.cs
+++ NGJ15/Assets/Scripts/Respawner.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class Respawner : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	private Collider box;
+	private Vector3 center;
+
+	void Start(){
+		box = this.GetComponentInChildren<Collider> ();
+		center = box.transform.position;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void OnTriggerEnter(Collider other)
+	{
+		Vector3 randPoint = new Vector3(Random.Range(center.x - (box.bounds.size.x/2),center.x + (box.bounds.size.x/2)),
+		                            Random.Range(center.y - (box.bounds.size.y/2),center.y + (box.bounds.size.y/2)),
+		                            Random.Range(center.z - (box.bounds.size.z/2),center.z + (box.bounds.size.z/2)));
+		other.transform.position = randPoint;
 	}
 }
