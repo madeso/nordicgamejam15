30cd71639344092bd1fc1298abea50a3dee5ee31
Anwe119 <rytter123>
2015-02-08 13:05:26 +0100

    now q resets

---
+++ NGJ15/Assets/Scripts/Resetter.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Resetter : MonoBehaviour {
+
+	void Update() {
+		if (Input.GetKeyDown ("q"))
+						Application.LoadLevel (Application.loadedLevelName);
+		
+	}
+}
