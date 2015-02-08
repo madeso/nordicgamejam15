33fb80192b7981d40a3c5d93b5b2a6f0536d66f8
Anwe119 <rytter123>
2015-02-08 12:41:21 +0100

    gravity ball

---
+++ NGJ15/Assets/Scripts/GraviSphere.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GraviSphere : MonoBehaviour {
+
+	// Use this for initialization
+	void OnCollisionEnter(Collision other){
+		if(other.transform.GetComponent<Usable>() != null){
+			other.transform.rigidbody.useGravity = !other.transform.rigidbody.useGravity;
+		}
+	}
+}
