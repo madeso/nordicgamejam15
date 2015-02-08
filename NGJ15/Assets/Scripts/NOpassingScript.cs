7126d0377e0612609e45216dcde9eb4a64c13cf1
Anwe119 <rytter123>
2015-02-08 11:08:03 +0100

    key no pass

---
+++ NGJ15/Assets/Scripts/NOpassingScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NOpassingScript : MonoBehaviour {
+
+	void OnCollisionEnter(Collision other){
+		if (other.transform.GetComponent<DoorScript> () != null) 
+		{
+			this.GetComponent<Flamable>().FlameTimer = 5.0f;
+			audio.Play();
+		}
+	}
+}
