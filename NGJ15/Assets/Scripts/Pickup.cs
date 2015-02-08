df9283e6af9687dd01b7115f5242b8ac696dc54b
Gustav <sir.gustav.the.coder@gmail.com>
2015-02-08 11:20:53 +0100

    picking up stuff is fun!!!!!

--- NGJ15/Assets/Scripts/Pickup.cs
+++ NGJ15/Assets/Scripts/Pickup.cs
@@ -14,13 +14,20 @@ public class Pickup : MonoBehaviour {
 	[SerializeField]
 	private float minThrowForce = 1;
 
-	private GameObject hand;
+	public GameObject hand {
+		get {
+			return this.transform.GetChild(0).gameObject;
+		}
+	}
 	private Camera camera;
 	private bool handsFull = false;
 	private GameObject item;
 	private float throwForce = 0;
 
-	private Vector3 handpos; 
+	public Vector3 handpos {
+		get {
+			return hand.transform.position; }
+	}
 
 	public bool HasPickedUpItem {
 		get {
@@ -39,16 +46,43 @@ public class Pickup : MonoBehaviour {
 		}
 	}
 
+
+
 	// Use this for initialization
 	void Start () {
-		hand = this.transform.GetChild(0).gameObject;
-		handpos = hand.transform.localPosition;
+
+		// handpos = hand.transform.localPosition;
 	}
+
+	public Vector3 debughand;
+
+	private float oldAngularDrag;
+
+	private float SlowDist = 1.0f;
+
+	private float MaxSpeed = 11;
+
+	private static Vector3 Arrival(Vector3 target, Vector3 position, float slowing_distance, float max_speed, Vector3 velocity) {
+		var target_offset = target - position;
+		var distance = target_offset.magnitude;
+		var ramped_speed = max_speed * (distance / slowing_distance);
+		var clipped_speed = Mathf.Min(ramped_speed, max_speed);
+		var desired_velocity = (clipped_speed / distance) * target_offset;
+		var steering = desired_velocity - velocity;
+		return steering;
+	}
+
 	
 	// shouold be remade into two functions so that it looks nice and needs to disable some stuff when picking up and putting down Also i have to mone it out a bit from the character so that you can allways move forward
 	void Update () {
+		this.debughand = this.item != null ? this.item.transform.localPosition : Vector3.zero;
 
-		if(Input.GetMouseButtonDown (1) && hand.transform.childCount !=0) // when we have an item and pres the left mouse button we set it free
+		if( this.item != null ) {
+			var lp = this.item.transform.localPosition;
+			// this.item.transform.localPosition = new Vector3(0, 0, 0);
+		}
+
+		if(Input.GetMouseButtonDown (1) && this.item != null) // when we have an item and pres the left mouse button we set it free
 		{
 			LetGo();
 			throwForce = 0; // resets force so that the player cannot save up force
@@ -60,14 +94,17 @@ public class Pickup : MonoBehaviour {
 			if (Physics.Raycast(transform.position, this.transform.forward, out hit, PickupDistance)) // sends a ray to find any object
 			{
 				Debug.Log(hit.transform.name);
-				if(hit.transform.GetComponent<Usable>() != null) // if we hit an object and it does not have a usable script on it, ignore it
+				var useable = hit.transform.GetComponent<Usable>();
+				if(useable != null) // if we hit an object and it does not have a usable script on it, ignore it
 				{
-				hit.transform.parent = hand.transform; // move in as a child of hand
-				hit.transform.localPosition = Vector3.zero;
-				item = hand.transform.GetChild(0).gameObject;   // the object
-				hit.rigidbody.constraints = RigidbodyConstraints.FreezeAll; // freez rotation and position of the rigidbody
-				hit.rigidbody.useGravity = false;  // disable gravity so that we can lift it
-				Physics.IgnoreCollision(item.collider, this.transform.parent.collider, true);
+					// useable.transform.parent = hand.transform; // move in as a child of hand
+					// useable.transform.localPosition = Vector3.zero;
+					// useable.transform.localPosition = Vector3.zero;
+					item = useable.gameObject;   // the object
+					// useable.rigidbody.isKinematic = true;
+					this.oldAngularDrag = this.item.rigidbody.angularDrag;
+					item.rigidbody.angularDrag = 120;
+					Physics.IgnoreCollision(item.collider, this.transform.parent.collider, true);
 				}
 			}
 			Debug.DrawRay(transform.position, this.transform.forward*PickupDistance, Color.green, 1f); // debug ray for seeing the distance
@@ -83,11 +120,24 @@ public class Pickup : MonoBehaviour {
 			Throw();
 		}
 
+		if( this.item != null ) {
+			// Vector3.SmoothDamp(
+			/*var diff = this.handpos - this.item.transform.position;
+			if( diff.magnitude > 0 ) {
+				diff *= diff.magnitude;
+			}*/
+			var diff = Arrival (this.handpos, this.item.transform.position, this.SlowDist, this.MaxSpeed, this.item.rigidbody.velocity);
+			diff *= 20.0f;
+
+			// this.item.rigidbody.useGravity = false;
+			this.item.rigidbody.AddForce(diff, ForceMode.Force);
+		}
+
 		//anything picked up???
-		if (hand.transform.childCount != 0)
+		if (this.item != null)
 		{
 			handsFull = true; //yes
-			item.rigidbody.velocity = Vector3.zero;
+			// item.rigidbody.velocity = Vector3.zero;
 		}
 		else
 		{
@@ -101,18 +151,19 @@ public class Pickup : MonoBehaviour {
 	{
 		if (item != null) 
 		{
-						item.rigidbody.useGravity = true;  //enables gravity
-						item.rigidbody.constraints = RigidbodyConstraints.None; //release all constrains
-						Physics.IgnoreCollision(item.collider, this.transform.parent.collider, false);
-						item.transform.parent = null;		 // set it free!!!
-						hand.transform.localPosition = handpos;
+			Physics.IgnoreCollision(item.collider, this.transform.parent.collider, false);
+			// item.transform.parent = null;		 // set it free!!!
+			// item.rigidbody.isKinematic = false;
+			// hand.transform.localPosition = handpos;
+			this.item.rigidbody.angularDrag = this.oldAngularDrag;
+			this.item = null;
 		}
 	}
 
 	private void Throw() // adds force before letting it go
 	{
+		// item.rigidbody.isKinematic = false;
 		item.rigidbody.AddForce (this.transform.forward*throwForce, ForceMode.Impulse);
-
 		//item.GetComponent<Usable> ().thrown = true;
 
 		LetGo ();
