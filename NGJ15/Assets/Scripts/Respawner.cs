439988e36cdcaed13dbd097294558b00e2700fb8
Aldwyns <aldwyns@email.com>
2015-02-08 12:25:39 +0100

    added reset box, removed teddy double mesh collider

--- NGJ15/Assets/Scripts/Respawner.cs
+++ NGJ15/Assets/Scripts/Respawner.cs
@@ -7,15 +7,16 @@ public class Respawner : MonoBehaviour {
 	private Vector3 center;
 
 	void Start(){
-		box = this.GetComponentInChildren<Collider> ();
+		box = this.transform.GetChild(0).GetComponent<Collider>();
 		center = box.transform.position;
 	}
 
 	void OnTriggerEnter(Collider other)
 	{
 		Vector3 randPoint = new Vector3(Random.Range(center.x - (box.bounds.size.x/2),center.x + (box.bounds.size.x/2)),
-		                            Random.Range(center.y - (box.bounds.size.y/2),center.y + (box.bounds.size.y/2)),
+		                           Random.Range(center.y - (box.bounds.size.y/2),center.y + (box.bounds.size.y/2)),
 		                            Random.Range(center.z - (box.bounds.size.z/2),center.z + (box.bounds.size.z/2)));
-		other.transform.position = randPoint;
+
+		                                other.transform.position = randPoint;
 	}
 }
